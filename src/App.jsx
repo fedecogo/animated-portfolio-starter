4f2ceb797bcc2863bdd7679a1bed796341321102
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-04-01 20:01:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+const App = () => {
+  return <div>
+    <section><a href="">Hero</a></section>
+    <section>Parallax</section>
+    <section>Service</section> 
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+
+    </div>;
+};
+
+export default App;
